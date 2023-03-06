17c80d9dc758ac3c74aae61a37b4c866b9740f70
Tanya <tanyakulemina94@gmail.com>
2023-03-06 12:17:51 +0300

    MakeMethod

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -5,4 +5,17 @@ string text = Console.ReadLine();
 //Разделяем текст по словам и оздаем массив строк
 string[] arrWords = text.Split(' ');
 
+//Создать метод, выбирающий слова из 3-ъ и менее символов
+void Select(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string word = array[i].ToString();
+        if (word.Length <= 3)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+    } 
+}
+
 
